d6e1db847b921b35124088b45bda75d64782a5cc
Beau <beau28713@gmail.com>
2022-06-14 05:45:56 -0400

    added ability to insert new parts and type hinting

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import pandas as pd
-import pymongo
 import json
 from pprint import pprint
 
 from client import MYDB, PARTS_COLL
+
 
 def insert_csv_sheet(csv_sheet):
     data = pd.read_csv(csv_sheet)
@@ -21,7 +21,8 @@ def test_collection():
     else:
         print("Test failed")
 
-def find_parts(query):
+
+def find_parts(query: dict):
 
     data = list(PARTS_COLL.find(query))
     if len(data) > 0:
@@ -31,19 +32,41 @@ def find_parts(query):
     else:
         print("Part not in system")
 
-def insert_part():
-    PARTS_COLL.insert_one()# Finish this next!
 
-def update_part(update_query, update_param, update_value):
-    PARTS_COLL.update_one({"Part Number": update_query}, {"$set": {update_param: update_value}})
+def insert_part(
+    part_number: str = "None given",
+    bin: str = "None given",
+    qty: str = "None given",
+    supplier: str = "None given",
+    description: str = "None given",
+    specification: str = "None given",
+):
+    PARTS_COLL.insert_one(
+        {
+            "Bin #": bin,
+            "Description": description,
+            "Part Number": part_number,
+            "Qty": qty,
+            "Specifications": specification,
+            "Supplier": supplier,
+        }
+    )
+
+
+def update_part(update_query: str, update_param: str, update_value: str):
+    PARTS_COLL.update_one(
+        {"Part Number": update_query}, {"$set": {update_param: update_value}}
+    )
     print(f"{update_query} has been updated")
+
 
 def main():
     # insert_csv_sheet("Parts_Inventory_List.csv")
     # test_collection()
-    find_parts({"Part Number": "171-009-203L001"})
+    # find_parts({"Part Number": "171-009-203L001"})
     # update_part("171-009-203L001", "Description", "9 Pin D-Sub F")
-    # insert_part()
+    insert_part("8675309", "1A", "3", "For a good time", "Jenny", "Stacy's Mom")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
